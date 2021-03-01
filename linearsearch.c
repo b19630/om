f01769c795ef1522c082c5eec25760fc98fda8fe
om <b19630@gmail.com>
2021-02-28 21:45:23 -0700

    hello

---
+++ linearsearch.c
@@ -0,0 +1,15 @@
+5plp5pyr]ge]g]rggktgkjhfkdwhlkdffdfhgeeprooooker[pogke#include<stdio.h>
+int main()
+{
+    int i,a[10],key,n;
+    printf("enter the size of element\n");
+    scanf("%d",&n);
+    printf("enter the elements\n");
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+
+}
+    printf("enter the element to be searched");
+    scanf("%d",&keys);
